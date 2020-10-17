c5c2eb3700d76537ba8c039be9fd068786460687
Varun Kulkarni <vskred126@gmail.com>
2020-10-16 21:50:01 -0400

    Added components directory with first component

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { FoodFindingMain } from './components/FoodFindingMain';
 
 export default function App() {
   return (
