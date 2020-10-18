f563bc69cfaa3346aceaf275c261abfff46d9b7e
Varun Kulkarni <vskred126@gmail.com>
2020-10-17 21:19:33 -0400

    Finished axios query for user Login/Authentication.  Need to implement transitions into next scene later

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,47 +3,47 @@ const User = require("../models/User");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    User.find()
+  User.find({ email: req.query['email'] })
     .then((data) => {
-        return res.status(200).json(data);
+      return res.status(200).json(data);
     })
     .catch((err) => res.status(500).json(err))
 });
 
 router.get("/:userid", (req, res) => {
-    User.findById(req.params.userid)
+  User.findById(req.params.userid)
     .then((data) => {
-        return res.status(200).json(data);
+      return res.status(200).json(data);
     })
     .catch((err) => res.status(500).json(err))
 });
 
 router.post("/signup", (req, res) => {
-    const user = new User({
-        role: req.body.role,
-        email: req.body.email,
-        password: req.body.password,
-        name: "",
-      });
-    
-      user
-        .save()
-        .then((data) => {
-          res.status(200).json(data);
-        })
-        .catch((err) => {
-          res.status(500).json(err);
-        });
-    
+  const user = new User({
+    role: req.body.role,
+    email: req.body.email,
+    password: req.body.password,
+    name: "",
+  });
+
+  user
+    .save()
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+
 });
 
 router.delete("/delete/:userid", (req, res) => {
-    User.remove({ _id: req.params.userid })
-      .then((data) => res.status(200).json(data))
-      .catch((err) => {
-        res.status(500).json(err);
-      });
-  });
-  
+  User.remove({ _id: req.params.userid })
+    .then((data) => res.status(200).json(data))
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+
 
 module.exports = router;
